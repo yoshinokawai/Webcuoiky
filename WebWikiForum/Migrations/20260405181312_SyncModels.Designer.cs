6b76ec5a20f599c18f9ecdd1deff488f10313cb1
yoshino <phamlinh2162005@gmail.com>
2026-04-06 01:28:21 +0700

    move database to somee

--- WebWikiForum/Migrations/20260405181312_SyncModels.Designer.cs
+++ WebWikiForum/Migrations/20260405181312_SyncModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebWikiForum.Data;
 
@@ -11,9 +12,11 @@ using WebWikiForum.Data;
 namespace WebWikiForum.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260405181312_SyncModels")]
+    partial class SyncModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebWikiForum/Migrations/20260405181312_SyncModels.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebWikiForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Agencies",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Agencies");
+        }
+    }
+}
