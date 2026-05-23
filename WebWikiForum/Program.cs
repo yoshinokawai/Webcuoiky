9886ff3cdb4b224784926c62d336b8a469039c08
yoshino <phamlinh2162005@gmail.com>
2026-05-23 23:37:25 +0700

    feat: initialize Program.cs with database migration, seed data, localization, and authentication services

--- WebWikiForum/Program.cs
+++ WebWikiForum/Program.cs
@@ -152,9 +152,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 app.UseRouting();
 
 var supportedCultures = new[] { "en", "vi", "ja" };
